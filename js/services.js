da1f0751d3891986682ce172198da56f5cf9097c
sunile <sunsunile21@gmail.com>
2017-04-13 14:16:00 +0800

    modify link

--- js/services.js
+++ js/services.js
@@ -2,7 +2,7 @@ angular.module('adminManageServices', [])
 
 .factory('Students', ['$resource',
     function($resource) {
-        return $resource('http://1.sunile22.applinzi.com/Home/student/:id', 
+        return $resource('http://2.backend23.applinzi.com/Home/student/:id', 
             { id: "@id" },
             {
             'get': { method: 'GET' },
@@ -17,7 +17,7 @@ angular.module('adminManageServices', [])
 
 .factory('Allinfo', ['$resource',function ($resource) {
    
-     return $resource('http://1.sunile22.applinzi.com/Home/allinfo/:academy_id/:department_id/:major_id', 
+     return $resource('http://2.backend23.applinzi.com/Home/allinfo/:academy_id/:department_id/:major_id', 
         {academy_id: "@academy_id",
         department_id:"@department",
         major_id:"@major_id"},
@@ -33,7 +33,7 @@ angular.module('adminManageServices', [])
 
 .factory('MajorCourse', ['$resource',function ($resource) {
    
-     return $resource('http://1.sunile22.applinzi.com/Home/course/:academy_id/:major_id/:student_id', 
+     return $resource('http://2.backend23.applinzi.com/Home/course/:academy_id/:major_id/:student_id', 
         {academy_id: "@academy_id",major_id:"@major_id",student_id:"@student_id"},
             {
             'get': { method: 'GET' },
@@ -47,7 +47,7 @@ angular.module('adminManageServices', [])
 
 .factory('MyCourse', ['$resource',function ($resource) {
    
-     return $resource('http://1.sunile22.applinzi.com/Home/mycourse/:academy_id/:major_id/:student_id', 
+     return $resource('http://2.backend23.applinzi.com/Home/mycourse/:academy_id/:major_id/:student_id', 
         {academy_id: "@academy_id",major_id:"@major_id",student_id:"@student_id"},
             {
             'get': { method: 'GET' },
@@ -61,7 +61,7 @@ angular.module('adminManageServices', [])
 
 .factory('CourContent', ['$resource',function ($resource) {
    
-     return $resource('http://1.sunile22.applinzi.com/Home/courContent/:course_id/:teacher_id', 
+     return $resource('http://2.backend23.applinzi.com/Home/courContent/:course_id/:teacher_id', 
         {course_id: "@course_id",teacher_id:"@teacher_id"},
             {
             'get': { method: 'GET' },
@@ -75,7 +75,7 @@ angular.module('adminManageServices', [])
 
 .factory('CourInfo', ['$resource',function ($resource) {
    
-     return $resource('http://1.sunile22.applinzi.com/Home/courInfo/:course_id/:teacher_id', 
+     return $resource('http://2.backend23.applinzi.com/Home/courInfo/:course_id/:teacher_id', 
         {course_id: "@course_id",teacher_id:"@teacher_id"},
             {
             'get': { method: 'GET' },
@@ -86,96 +86,3 @@ angular.module('adminManageServices', [])
             'delete': { method: 'DELETE' }
         });
 }])
-
-
-
-
-// .factory('Students', ['$resource',
-//     function($resource) {
-//         return $resource('http://1.sunile22.applinzi.com/Home/student/:id', 
-//             { id: "@id" },
-//             {
-//             'get': { method: 'GET' },
-//             'save': { method: 'POST' },
-//             'query': { method: 'GET', isArray: true },
-//             'remove': { method: 'DELETE' },
-//             'update':{method:'PUT'},
-//             'delete': { method: 'DELETE' }
-//         });
-//     }
-// ])
-
-// .factory('Allinfo', ['$resource',function ($resource) {
-   
-//      return $resource('http://1.sunile22.applinzi.com/Home/allinfo/:academy_id/:department_id/:major_id', 
-//         {academy_id: "@academy_id",
-//         department_id:"@department",
-//         major_id:"@major_id"},
-//             {
-//             'get': { method: 'GET'},
-//             'save': { method: 'POST'},
-//             'query': { method: 'GET'},
-//             'remove': { method: 'DELETE'},
-//             'update':{method:'PUT'},
-//             'delete': { method: 'DELETE'}
-//         });
-// }])
-
-// .factory('MajorCourse', ['$resource',function ($resource) {
-   
-//      return $resource('http://1.sunile22.applinzi.com/Home/course/:academy_id/:major_id/:student_id', 
-//         {academy_id: "@academy_id",major_id:"@major_id",student_id:"@student_id"},
-//             {
-//             'get': { method: 'GET' },
-//             'save': { method: 'POST' },
-//             'query': { method: 'GET', isArray: true },
-//             'remove': { method: 'DELETE' },
-//             'update':{method:'PUT'},
-//             'delete': { method: 'DELETE' }
-//         });
-// }])
-
-// .factory('MyCourse', ['$resource',function ($resource) {
-   
-//      return $resource('http://1.sunile22.applinzi.com/Home/mycourse/:academy_id/:major_id/:student_id', 
-//         {academy_id: "@academy_id",major_id:"@major_id",student_id:"@student_id"},
-//             {
-//             'get': { method: 'GET' },
-//             'save': { method: 'POST' },
-//             'query': { method: 'GET'},
-//             'remove': { method: 'DELETE' },
-//             'update':{method:'PUT'},
-//             'delete': { method: 'DELETE' }
-//         });
-// }])
-
-// .factory('CourContent', ['$resource',function ($resource) {
-   
-//      return $resource('http://1.sunile22.applinzi.com/Home/courContent/:course_id/:teacher_id', 
-//         {course_id: "@course_id",teacher_id:"@teacher_id"},
-//             {
-//             'get': { method: 'GET' },
-//             'save': { method: 'POST' },
-//             'query': { method: 'GET'},
-//             'remove': { method: 'DELETE' },
-//             'update':{method:'PUT'},
-//             'delete': { method: 'DELETE' }
-//         });
-// }])
-
-// .factory('CourInfo', ['$resource',function ($resource) {
-   
-//      return $resource('http://1.sunile22.applinzi.com/Home/courInfo/:course_id/:teacher_id', 
-//         {course_id: "@course_id",teacher_id:"@teacher_id"},
-//             {
-//             'get': { method: 'GET' },
-//             'save': { method: 'POST' },
-//             'query': { method: 'GET'},
-//             'remove': { method: 'DELETE' },
-//             'update':{method:'PUT'},
-//             'delete': { method: 'DELETE' }
-//         });
-// }])
-
-
-
